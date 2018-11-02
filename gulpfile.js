ff247b613bf0741d6eb749837d62512882cfc47c
gonzalo.martinez <gonzalo.martinez@imatia.com>
2018-11-02 14:28:53 +0100

    Updating configuration files

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,28 @@
 const gulp = require('gulp');
 const sass = require('node-sass');
 const inlineTemplates = require('gulp-inline-ng2-template');
-const exec = require('child_process').exec;
+// const exec = require('child_process').exec;
 const copyfiles = require('copyfiles');
 const cssimport = require("gulp-cssimport");
-const replace = require('gulp-replace');
-const ext_replace = require('gulp-ext-replace');
+// const replace = require('gulp-replace');
+const replace = require('gulp-ext-replace');
 
 const SCSS_CONF = {
   SRC: './tmp/styles.scss',
   DIST: './dist'
 };
 
-gulp.task('styles', ['map-styles-copy'], (callback) => {
+gulp.task('map-styles-copy', (callback) => {
+  copyfiles(['./styles.scss', './tmp'], true, callback);
+});
+
+gulp.task('styles-creation', (callback) => {
   return gulp.src(SCSS_CONF.SRC)
     .pipe(cssimport(SCSS_CONF.OPTIONS))
     .pipe(gulp.dest(SCSS_CONF.DIST));
 });
 
-gulp.task('map-styles-copy', (callback) => {
-  copyfiles(['./styles.scss', './tmp'], true, callback);
-});
+gulp.task('styles', gulp.series('map-styles-copy', 'styles-creation'));
 
 const FILES = [
   'CHANGELOG.md',
@@ -32,32 +34,9 @@ const FILES = [
   'dist'
 ];
 
-gulp.task('copy-files', ['copy-files-array', 'copy-dependencies-assets']);
-
-gulp.task('copy-files-array', (callback) => {
-  copyfiles(FILES, true, callback);
-});
-
 /**
  * DEPENDENCIES
  */
-gulp.task('copy-dependencies-assets', ['copy.leaflet.assets', 'copy.leaflet.draw.assets'], (callback) => {
-  gulp.src('./tmp/assets/dependencies/**/*.css')
-    .pipe(ext_replace('.scss'))
-    .pipe(gulp.dest('./tmp/assets/dependencies/'))
-});
-
-const LEAFLET_FILES = [
-  'node_modules/leaflet/dist/leaflet.css',
-  'node_modules/leaflet.markercluster/dist/MarkerCluster.css',
-  'node_modules/leaflet.markercluster/dist/MarkerCluster.Default.css',
-  'tmp/assets/dependencies/leaflet'
-];
-
-gulp.task('copy.leaflet.assets', ['copy.leaflet.images'], (callback) => {
-  copyfiles(LEAFLET_FILES, true, callback);
-});
-
 const LEAFLET_IMAGES = [
   'node_modules/leaflet/dist/images/*',
   'dist/images'
@@ -67,18 +46,32 @@ gulp.task('copy.leaflet.images', (callback) => {
   copyfiles(LEAFLET_IMAGES, true, callback);
 });
 
-/**
- * LEAFLET DRAW
- */
-const LEAFLET_DRAW_FILES = [
-  'node_modules/leaflet-draw/dist/leaflet.draw.css',
+
+const LEAFLET_FILES = [
+  'node_modules/leaflet/dist/leaflet.css',
+  'node_modules/leaflet.markercluster/dist/MarkerCluster.css',
+  'node_modules/leaflet.markercluster/dist/MarkerCluster.Default.css',
   'tmp/assets/dependencies/leaflet'
 ];
 
-gulp.task('copy.leaflet.draw.assets', ['copy.leaflet.draw.images'], (callback) => {
-  copyfiles(LEAFLET_DRAW_FILES, true, callback);
+gulp.task('copy.leaflet.assets', (callback) => {
+  copyfiles(LEAFLET_FILES, true, callback);
 });
 
+
+gulp.task('copy-dependencies-assets', (callback) => {
+  return gulp.src('./tmp/assets/dependencies/**/*.css')
+    .pipe(replace('.scss'))
+    .pipe(gulp.dest('./tmp/assets/dependencies/'))
+});
+
+gulp.task('copy-files-array', (callback) => {
+  copyfiles(FILES, true, callback);
+});
+
+/**
+ * LEAFLET DRAW
+ */
 const LEAFLET_DRAW_IMAGES = [
   'node_modules/leaflet-draw/dist/images/*',
   'dist/images'
@@ -88,10 +81,44 @@ gulp.task('copy.leaflet.draw.images', (callback) => {
   copyfiles(LEAFLET_DRAW_IMAGES, true, callback);
 });
 
+const LEAFLET_DRAW_FILES = [
+  'node_modules/leaflet-draw/dist/leaflet.draw.css',
+  'tmp/assets/dependencies/leaflet'
+];
+
+gulp.task('copy.leaflet.draw.assets', (callback) => {
+  copyfiles(LEAFLET_DRAW_FILES, true, callback);
+});
+
+
+gulp.task('copy-files',
+  gulp.series('copy-files-array',
+    'copy.leaflet.images',
+    'copy.leaflet.assets',
+    'copy.leaflet.draw.images',
+    'copy.leaflet.draw.assets',
+    'copy-dependencies-assets')
+);
+
+
 /**
  * Inline templates configuration.
  * @see  https://github.com/ludohenin/gulp-inline-ng2-template
  */
+
+/**
+* Compile SASS to CSS.
+* @see https://github.com/ludohenin/gulp-inline-ng2-template
+* @see https://github.com/sass/node-sass
+*/
+function compileSass(path, ext, file, callback) {
+  let compiledCss = sass.renderSync({
+    file: path,
+    outputStyle: 'compressed',
+  });
+  callback(null, compiledCss.css);
+}
+
 const INLINE_TEMPLATES_CONF = {
   SRC: ['./**/*.ts', '!./tmp/**/*', '!./node_modules/**/*', '!./custom-typings.d.ts'],
   DIST: './tmp',
@@ -112,16 +139,3 @@ gulp.task('inline-templates', () => {
     .pipe(inlineTemplates(INLINE_TEMPLATES_CONF.CONFIG))
     .pipe(gulp.dest(INLINE_TEMPLATES_CONF.DIST));
 });
-
-/**
- * Compile SASS to CSS.
- * @see https://github.com/ludohenin/gulp-inline-ng2-template
- * @see https://github.com/sass/node-sass
- */
-function compileSass(path, ext, file, callback) {
-  let compiledCss = sass.renderSync({
-    file: path,
-    outputStyle: 'compressed',
-  });
-  callback(null, compiledCss.css);
-}
