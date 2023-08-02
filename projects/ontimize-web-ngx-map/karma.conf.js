74d4a858b807123e120654e0cb2533fce04edde7
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2023-08-02 13:35:36 +0200

    Updated karma.conf.js

--- projects/ontimize-web-ngx-map/karma.conf.js
+++ projects/ontimize-web-ngx-map/karma.conf.js
@@ -9,17 +9,21 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-sonarqube-unit-reporter')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, '../../coverage/ontimize-web-ngx-map'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'lcovonly' },
+        { type: 'text-summary' }
+      ]
     },
     sonarQubeUnitReporter: {
       sonarQubeVersion: 'LATEST',
